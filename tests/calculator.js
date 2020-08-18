7e81c9eff652296ef2268dfd426bc38cfdc0806f
Mohammed Shahin Uddin <mo@thetestroom.com>
2020-08-18 22:57:36 +0100

    Example test file with Page Object Model

---
+++ tests/calculator.js
@@ -0,0 +1,17 @@
+let homepage = require('../pages/homepage')
+
+describe('protractor demo app for calculator', function(){
+
+    beforeEach(() => {
+        homepage.nagivateTo('http://juliemr.github.io/protractor-demo/');
+    })
+
+    it('adding numbers', function(){
+        homepage.enterFirstNumber(6);
+        homepage.enterSecondNumber(4);
+        homepage.clickGo();
+        
+        expect(homepage.getResult()).toEqual('10');
+    });
+
+});
