cd24e425cbe500ca4ce655f3a36588ee40b02656
Mohammed Shahin Uddin <mo@thetestroom.com>
2020-08-24 00:43:25 +0100

    Added HTML report generation with screenshots on failure

--- tests/calculator.js
+++ tests/calculator.js
@@ -14,4 +14,12 @@ describe('protractor demo app for calculator', function(){
         expect(homepage.getResult()).toEqual('10');
     });
 
+    it('adding numbers', function(){
+        homepage.enterFirstNumber(6);
+        homepage.enterSecondNumber(4);
+        homepage.clickGo();
+        
+        expect(homepage.getResult()).toEqual('11');
+    });
+
 });
